7b963e90ca6bd960d5191304b6b2d3a0af97784e
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-27 21:37:03 +0000

    let answerValue
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,4 @@ const controls = document.querySelector(".controls-container");
 const result = document.getElementById("result");
 const submitBtn = document.getElementById("submit-btn");
 const errorMessage = document.getElementById("error-msg");
+let answerValue;
