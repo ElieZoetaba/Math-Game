647c51b261ba4206c8428f11f45f483c94d4c8f1
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-27 21:35:19 +0000

    const submitBtn
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,4 @@ const startBtn = document.getElementById("start-btn");
 const question = document.getElementById("question");
 const controls = document.querySelector(".controls-container");
 const result = document.getElementById("result");
+const submitBtn = document.getElementById("submit-btn");
