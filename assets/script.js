d3a70c5e8e9edc97f9a1ac5b4ee0c0a4b95a7d81
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-27 21:34:14 +0000

    const result
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
 let operators = ["+", "-", "*"];
 const startBtn = document.getElementById("start-btn");
 const question = document.getElementById("question");
+const controls = document.querySelector(".controls-container");
+const result = document.getElementById("result");
