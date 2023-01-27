a6b0abf35eb54a5aeae116728237ad9a92298c9c
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-27 21:40:08 +0000

    Two random values between 1 and 20
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,9 @@ let operatorQuestion;
 
 //Random Value Generator
 const randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const questionGenerator = () => {
+    //Two random values between 1 and 20
+    let [num1, num2] = [randomValue(1, 20), randomValue(1, 20)];
+
+}
