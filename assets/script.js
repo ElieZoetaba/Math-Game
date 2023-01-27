7046f1f3fe8f0fba8e8232576e99c4b6ee257e50
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-27 21:35:43 +0000

    const errorMessage
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,4 @@ const question = document.getElementById("question");
 const controls = document.querySelector(".controls-container");
 const result = document.getElementById("result");
 const submitBtn = document.getElementById("submit-btn");
+const errorMessage = document.getElementById("error-msg");
