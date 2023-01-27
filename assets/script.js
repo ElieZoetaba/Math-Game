034a662a67be4ac0a6ec2a68daef8223de7bdbd9
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-27 21:49:50 +0000

    For placing the input at random position
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -24,4 +24,8 @@ const questionGenerator = () => {
 
   //Solve equation
   let solution = eval(`${num1}${randomOperator}${num2}`);
+
+  //For placing the input at random position
+  //(1 for num1, 2 for num2, 3 for operator, anything else(4) for solution)
+  let randomVar = randomValue(1, 5);
 };
